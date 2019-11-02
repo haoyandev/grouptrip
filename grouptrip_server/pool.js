d5bd2fc030d605976de6f33b3725fd0dd33f1fd5
wy450150 <50820058@qq.com>
2019-11-02 09:25:31 +0800

    img

--- grouptrip_server/pool.js
+++ grouptrip_server/pool.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 var pool = mysql.createPool({
-  host: 'localhost',
+  host: 'w.rdc.sae.sina.com.cn',
   port: 3306,
-  user: 'root',
-  password: '',
-  database: 'trip',
+  user: 'kwlk51lm3m',
+  password: '4j1lwjyz1hh5w0x51xl11jl00m0wxkxhhhxxkllk',
+  database: 'app_grouptrip',
   connectionLimit: 15
 })
 
